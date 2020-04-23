aa79d64a8639b1415d9be6c5a570fa8cf062c37e
susumoa <tobiasbori@gmail.com>
2020-04-23 19:17:37 +0100

    feat: click on a tag smoothly scrolls to section

--- js/app.js
+++ js/app.js
@@ -1,44 +1,18 @@
-/**
- *
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- *
- * Dependencies: None
- *
- * JS Version: ES2015/ES6
- *
- * JS Standard: ESlint
- *
- */
-
-/**
- * Define Global Variables
- *
- */
-
 const fragment = document.createDocumentFragment();
-
 const sections = document.querySelectorAll('section[data-nav]');
-
 const navbarList = document.getElementById('navbar__list');
 
-/**
- * End Global Variables
- * Start Helper Functions
- *
- */
-
 function createNewListElement(el) {
-  const link = document.createElement('a');
-  link.setAttribute('href', `#${el.getAttribute('id')}`);
-
   const listitem = document.createElement('li');
-  listitem.textContent = el.getAttribute('data-nav');
-  link.appendChild(listitem);
+  const link = document.createElement('a');
 
-  fragment.appendChild(link);
+  link.setAttribute('href', `#${el.getAttribute('id')}`);
+  link.setAttribute('class', 'menu__link');
+
+  link.textContent = el.getAttribute('data-nav');
+
+  listitem.appendChild(link);
+  fragment.appendChild(listitem);
 }
 
 function isInViewport(el) {
@@ -49,40 +23,38 @@ function isInViewport(el) {
   );
 }
 
-/**
- * End Helper Functions
- * Begin Main Functions
- *
- */
-
-// build the nav
+// navigation
 
 sections.forEach((el) => createNewListElement(el));
-
 navbarList.appendChild(fragment);
 
 // Add class 'active' to section when near top of viewport
 
-document.addEventListener('scroll', (e) => {
+function toggleActiveClass() {
   sections.forEach((el) => {
     if (isInViewport(el)) {
-      el.classList.toggle('your-active-class');
+      el.classList.add('your-active-class');
     } else {
       el.classList.remove('your-active-class');
     }
   });
-});
+}
 
 // Scroll to anchor ID using scrollTO event
-
-/**
- * End Main Functions
- * Begin Events
- *
- */
-
-// Build menu
+function scrollToId(section) {
+  section.scrollIntoView({ behavior: 'smooth' });
+}
 
 // Scroll to section on link click
+document.addEventListener('click', (e) => {
+  if (e.target.nodeName === 'A') {
+    e.preventDefault();
+    const sec = document.querySelector(e.target.getAttribute('href'));
+    scrollToId(sec);
+  }
+});
 
 // Set sections as active
+document.addEventListener('scroll', (e) => {
+  toggleActiveClass();
+});
